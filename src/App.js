540e4f2705788b3a70fe0a32bf1926f7dae9156d
Cem Gezer <alicemgzr74@gmail.com>
2024-09-02 10:53:07 +0300

    contact components created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 // components
-import About from './components/About';
-import Banner from './components/Banner';
-import Contact from './components/Contact';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Services from './components/Services';
-import Work from './components/Work';
+import About from "./components/About";
+import Banner from "./components/Banner";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Services from "./components/Services";
+import Work from "./components/Work";
 
 function App() {
   return (
-    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+    <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
       <Header />
       <Banner />
       <Nav />
@@ -18,7 +18,6 @@ function App() {
       <Services />
       <Work />
       <Contact />
-      <div className='h-[4000px]'></div>
     </div>
   );
 }
